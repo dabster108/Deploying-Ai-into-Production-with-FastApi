570d95196b076fb73463b629c2bc60c11bbf7753
Dikshanta Chapagain <3108dikshanta@gmail.com>
2024-12-06 10:44:28 +0545

    new file added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 # fast api is an object and we are first creating the instance of the object 
@@ -25,5 +25,14 @@ def index():
 
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int):   # giving the student id with the datatype
+def get_student(student_id: int = Path(..., description="input the Id of the student", gt = 0 , lt = 5)):
     return students[student_id]
+
+# gt - greater than , lt - lesser than , ge - greater than equals to , le - less than or equals to 
+# query parameter is used to pass a value into a url 
+@app.get("/get-by-name")
+def get_student(name:str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data" : "Not found"}
