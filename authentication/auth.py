30dc327574719704866dd865ab5b0145a0ac919e
Dikshanta <3108dikshanta@gmail.com>
2025-03-23 17:01:50 +0545

    Rate limiter added

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import APIKeyHeader
+from collections import defaultdict
 
 auth = FastAPI()
 
@@ -17,3 +18,17 @@ def read_items(api_key: str = Depends(header_scheme)):
         raise HTTPException(status_code=403, detail="Invalid or missing API key")
     
     return {"message": "Access granted", "items": ["item1", "item2", "item3"]}
+
+
+
+#lass RateLimiter:
+    def __init__(self, request_per_min: int = 10):
+        self.request_per_min = request_per_min
+        self.requests = defaultdict(list)  # Corrected defaultdict usage
+
+    def is_rate_limited(self, api_key: str) -> Tuple[bool, int]:  # Fixed indentation and type hint
+        current_time = time.time()
+        request_times = self.requests[api_key]
+
+
+#rate_limiter = RateLimiter(Request_per_min = 10)
